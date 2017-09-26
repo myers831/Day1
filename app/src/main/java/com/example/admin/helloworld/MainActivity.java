a5e248d988b92029ce5efcf7e13f7cb1387593c1
Mac <myers831@yahoo.com>
2017-09-26 17:40:18 -0400

    day 1 code

--- app/src/main/java/com/example/admin/helloworld/MainActivity.java
+++ app/src/main/java/com/example/admin/helloworld/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.admin.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etUpdateValue;
+    Button btnUpdateTextView;
+    TextView tvUpdatedValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUpdateValue = (EditText) findViewById(R.id.etInputValue);
+        btnUpdateTextView = (Button) findViewById(R.id.btnUpdateTextView);
+        tvUpdatedValue = (TextView) findViewById(R.id.tvUpdatedValue);
+
+        btnUpdateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvUpdatedValue.setText(etUpdateValue.getText().toString());
+            }
+        });
     }
 }
