93a4652dcd0e6c03d7ec681bd169677a9f1796f3
Mac <myers831@yahoo.com>
2017-09-26 15:31:38 -0400

    day 1 code

---
+++ app/src/main/java/com/example/admin/helloworld/CodingTest.java
@@ -0,0 +1,46 @@
+package com.example.admin.helloworld;
+
+/**
+ * Created by Admin on 9/26/2017.
+ */
+
+public class CodingTest {
+
+    public static void main(String[] args) {
+        //create s function to check if the number
+        //is divisible by 3 then print "fizz"
+        //is divisible by 5 then print "buzz"
+        //is divisible by 3 and 5 then print "fizz buzz"
+        //otherwise print itself
+
+        String [] animals = {"cat", "hen","cat", "cow", "dog", "pig", "cow", "dog", "rat"};
+
+        fizzbuzz(30);
+        findDulicates(animals);
+    }
+
+    public static void fizzbuzz(int num){
+        if(num%5 == 0 && num%3 == 0){
+            System.out.println("fizzbuzz");
+        }
+        else if(num%5 == 0){
+            System.out.println("buzz");
+        }else if(num%3 == 0){
+            System.out.println("fizz");
+        }else{
+            System.out.println(num);
+        }
+    }
+
+    public static void findDulicates(String [] stringList){
+        for(int i = 0; i<stringList.length; i++){
+            for(int j = 0; j<stringList.length; j++){
+                if (i == j){
+                    j++;
+                }else if(stringList[i].equals(stringList[j])){
+                    System.out.println(stringList[i]);
+                }
+            }
+        }
+    }
+}
